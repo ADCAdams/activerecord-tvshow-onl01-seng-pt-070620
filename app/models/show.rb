9f4aafa2bf1827fbd20030dedc26c3518047d521
Andy Adams <andrewdcadams@gmail.com>
2020-10-02 18:29:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,18 @@
 class Show < ActiveRecord::Base 
 
   def self.highest_rating
-    self.maximum(rating_)
+    self.maximum(rating)
   end
   
+  def self.most_popular_show
+    self.where("rating = highest_rating", title)
+  end
+  
+  def self.lowest_rating
+    self.minimum(rating)
+  end
+  
+  def self.least_popular_show
+    self.where("rating = highest_rating", title)
+  end
 end
